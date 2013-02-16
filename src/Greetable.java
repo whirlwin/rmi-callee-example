427eaec6bfa11368f94f82b1fde72b1d5fd0077e
whirlwin <whirlwin90@gmail.com>
2013-02-17 00:28:15 +0100

    Initial commit.

---
+++ src/Greetable.java
@@ -0,0 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Greetable extends Remote {
+
+    String greet() throws RemoteException;
+}
---
+++ src/Greeter.java
@@ -0,0 +1,17 @@
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+class Greeter extends UnicastRemoteObject implements Greetable {
+
+    public Greeter() throws RemoteException { super(); }
+
+    @Override
+    public String greet() throws RemoteException { return "Hi"; }
+
+    public static void main(String[] args) throws Exception {
+
+        // Bind to rmiregistry
+        Naming.bind("rmi://localhost/Greeter", new Greeter());
+    }
+}
