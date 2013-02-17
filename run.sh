909c6878111e2ae3f436ae8fb290f50c059b602f
whirlwin <whirlwin90@gmail.com>
2013-02-17 14:01:20 +0100

    Working example.

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-java -c
+java -cp bin/\
+     -Djava.rmi.server.codebase=file:$PWD/g.jar\
+     Greeter
+#-Djava.security.policy=all.policy\
--- src/Greeter.java
+++ src/Greeter.java
@@ -1,3 +1,4 @@
+
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
